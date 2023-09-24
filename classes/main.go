1ee7f26854bb757fabde9e0b86ca9b5b3c91d685
Mikhail Anishchenko <misha321654897i@gmail.com>
2023-09-24 18:24:04 +0300

    classes fix

--- classes/main.go
+++ classes/main.go
@@ -1,42 +1,113 @@
 package main
 
-import "fmt"
-
-type Adress struct {
-	city   string
-	street string
-}
+import (
+	"fmt"
+	"reflect"
+)
 
 type User struct {
-	firstName string
+	firstName string `default:"Anton"`
 	lastName  string
 	email     string
 	age       int
-	Adress    Adress
+}
+
+type Adress struct {
+	country string
+	city    string
+}
+
+// type User struct {
+// 	firstName string `default:"AAA"`
+// 	lastName  string
+// 	email     string
+// 	age       int
+// 	Adress    Adress
+// }
+
+func NewAdress(city string, street string) *Adress {
+	return &Adress{
+		country: city,
+		city:    street,
+	}
+}
+
+func print_user(u *User) {
+	fmt.Printf("name: %s %s\n", u.firstName, u.lastName)
+	fmt.Printf("email: %s\n", u.email)
+	fmt.Printf("age: %d\n", u.age)
 }
 
 func (u *User) print_me() {
-	fmt.Println(*u)
+	fmt.Printf("name: %s %s\n", u.firstName, u.lastName)
+	fmt.Printf("email: %s\n", u.email)
+	fmt.Printf("age: %d\n", u.age)
 }
 
 func (u *User) SetName(name string) {
 	u.firstName = name
 }
 
-func print_user(u1 *User, u2 User) {
-	fmt.Println(u1.firstName, "\n", u2.firstName)
+// func NewUser(
+// 	lastName string,
+// 	email string,
+// 	age int,
+// ) *User {
+// 	user := User{
+// 		lastName: lastName,
+// 		email:    email,
+// 		age:      age,
+// 	}
+// 	user.firstName = "Anton"
+// 	return &user
+// }
+
+func NewUser(
+	lastName string,
+	email string,
+	age int,
+) *User {
+	user := User{
+		lastName: lastName,
+		email:    email,
+		age:      age,
+	}
+	typ := reflect.TypeOf(user)
+	if user.firstName == "" {
+		f, _ := typ.FieldByName("firstName")
+		user.firstName = f.Tag.Get("default")
+	}
+	return &user
 }
 
-// func NewUser(...) *User{
-// 	user := User{...}
-
+// func NewUser(firstName string,
+// 	lastName string,
+// 	email string,
+// 	age int,
+// ) *User {
+// 	user := User{
+// 		firstName: firstName,
+// 		lastName:  lastName,
+// 		email:     email,
+// 		age:       age,
+// 	}
+// 	user.Adress = *NewAdress("Russia", "Moscow")
+// 	return &user
 // }
+
+type Website struct {
+	url             string
+	users_per_month int
+}
 
 type Admin struct {
 	IsAdmin bool
 	User
-	parent User
+	department string
+	Website
 }
+
+// func NewAdmin()
 
 // func (u *Admin) print_me() {
 // 	fmt.Printf("i'm an admin and my name is %s %s", u.firstName, u.lastName)
@@ -44,9 +115,29 @@ type Admin struct {
 // }
 
 func main() {
-	user1 := User{firstName: "Misha", lastName: "A", email: "exampple@g.com", age: 20}
-	user2 := User{firstName: "Boris", lastName: "B", email: "exampple@g.com", age: 22}
-	print_user(&user1, user2)
+	user := NewUser("A", "exampple@g.com", 20)
+	fmt.Println(user.firstName)
+
+	ad := Admin{true, *user, "articles", Website{"url", 200}}
+	fmt.Println(ad.users_per_month)
+
+	// admin := Admin{true, *user, "articles"}
+	// fmt.Printf("Hello! I'm %s %s and I administer the %s section",
+	// 	admin.firstName, admin.lastName, admin.department)
+
+	// fmt.Printf("user's adress is %s, %s", user.Adress.country, user.Adress.city)
+
+	// user2 := User{lastName: "B", email: "exampple@g.com", age: 22}
+
+	// user1 := NewUser("Misha", "A", "example@g.com", 20)
+	// user1.print_me()
+	// user1.SetName("Borya")
+	// user1.print_me()
+
+	// user2 := User{firstName: "Boris", lastName: "B", email: "exampple@g.com", age: 22}
+
+	// print_user(user1, user2)
+	// user1.SetName("kola")
 	// user1.print_me()
 	// user2.print_me()
 	// adm := Admin{true, user1, user2}
